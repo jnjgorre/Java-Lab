f3ea4900f28e2d657a71713bf375b3c7051ff206
jnjgorre <108084644+jnjgorre@users.noreply.github.com>
2022-07-10 18:17:19 -0700

    first commit being made

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int availablePlaneSeats = 5;
+		
+		
+		
+		double costOfGroceries = 23.64;
+		
+		
+		
+		char middleInitial = 'N';
+
+		
+		
+		boolean isHotOutside = true;
+		
+		
+		String customerFirstName = "Sam";
+		
+		
+		String streetAddress = "1234 N Marvel St";
+		
+		
+		System.out.println("Available seats left on the plane: " + availablePlaneSeats);
+		System.out.println(costOfGroceries + " is the total price for groceries");
+		System.out.println("The person's middle initial is " + middleInitial);
+		System.out.println("It is hot outside - " + isHotOutside);
+		System.out.println("The customers first name is " + customerFirstName);
+		System.out.println(streetAddress + " is where the person lives");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		 
+		 
+
+	
+		
+		
+		
+
+	}
+
+}
